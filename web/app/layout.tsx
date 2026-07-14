d15a9f2cb67664ca13559a9c7450c8915b03b0ba
ArhamKhan45 <karham45@gmail.com>
2026-07-14 12:55:32 +0530

    wip

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
-import keepServerAlive from "@/src/utils/keepAlive";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +28,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  keepServerAlive.start();
   return (
     <html
       lang="en"
